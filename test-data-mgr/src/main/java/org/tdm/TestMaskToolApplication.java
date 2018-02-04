445c8c66a7cd5b6e3a75a677c96a8e1f33ae2248
NEERAJ SUTHAR <suthar.nrj@gmail.com>
2018-02-04 23:05:33 +0530

    0.0.1-SNAPSHOT
    *Properties file added.

--- test-data-mgr/src/main/java/org/tdm/TestMaskToolApplication.java
+++ test-data-mgr/src/main/java/org/tdm/TestMaskToolApplication.java
@@ -3,7 +3,11 @@ package org.tdm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.tdm.encryption.RandomNameGenerator;
+import org.tdm.properties.TDMProperties;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -11,7 +15,20 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class TestMaskToolApplication {
 
 	public static void main(String[] args) {
+		
+		ApplicationContext ct = new AnnotationConfigApplicationContext(TDMProperties.class);
+		
+		/*AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+			 ctx.register(TDMProperties.class);
+			 ctx.refresh(); */
+			 
 		SpringApplication.run(TestMaskToolApplication.class, args);
+		
+		 RandomNameGenerator gen = new RandomNameGenerator();
+		 System.out.println(gen.genarateFirstName() + " " + gen.genarateLastName());
+		
+		
+		
 	}
 	
 	
